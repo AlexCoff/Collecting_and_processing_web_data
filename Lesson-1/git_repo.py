bcb9779df2c64b50afdf5a7968668ff266cffc19
Alexander Korovin <aakorovin@me.com>
2020-05-16 14:37:17 +0300

    1st complete ++

--- Lesson-1/git_repo.py
+++ Lesson-1/git_repo.py
@@ -12,16 +12,25 @@ header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:68.0) Gecko/2010
         'Accept':'*/*'}
 response = requests.get(main_link + '/users/' + user_name + '/repos',headers=header)
 if response.ok:
-    data = json.loads(response.text)
-    if len(data)==0:
-        print(f"User {user_name} have not public repos")
-    else:
-        print(f"For user {user_name} list of public repository:")
-        for i in range(0,len(data)):
-                print(data[i]['name'])
+        data = json.loads(response.text)
+        if len(data)==0:
+                print(f"User {user_name} have not public repos")
+        else:
+                print(f"For user {user_name} list of public repository:")
+                repo_data = []
+                for i in range(0,len(data)):
+                        repo = {}
+                        repo['name'] = data[i]['name']
+                        repo['url'] = data[i]['html_url']
+                        repo['create_date'] = data[i]['created_at']
+                        repo_data.append(repo)
+                        print(data[i]['name'])
+                #Write full data in file
+                with open( 'full_data_to_user_' + user_name + '.json', 'w') as json_file:
+                        json.dump(data, json_file)
+                #Write only names, urls, create data
+                with open( user_name + '.json', 'w') as json_file:
+                        json.dump(repo_data, json_file)
+
 else:
         print('Incorrect input, or user not exist')
-
-#Write full data in file
-with open(  user_name + '.json', 'w') as json_file:
-    json.dump(data, json_file)
