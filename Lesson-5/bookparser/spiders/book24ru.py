23446d9f3c4f87a557fbdfa64f7e91489124366a
Alexander Korovin <aakorovin@me.com>
2020-05-31 15:02:24 +0300

    Init

---
+++ Lesson-5/bookparser/spiders/book24ru.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class Book24ruSpider(scrapy.Spider):
+    name = 'book24ru'
+    allowed_domains = ['book24.ru']
+    start_urls = ['http://book24.ru/']
+
+    def parse(self, response):
+        pass
